b794be7ed34bf9bf6d1b1a7f7d81b32ce1fac38f
2557800625@qq.com <42715287+SEUChenyangWei@users.noreply.github.com>
2021-10-31 22:53:43 +0800

    StringCrypto added and verified

--- CryptologyProj.py
+++ CryptologyProj.py
@@ -1,234 +1,203 @@
-import base64
-xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
+import AES
+import DES
+import MD5
+import SHA
+import RSA
+import AllConvert
+import random
 
-keyLength2roundNum = {16:10, 24:12, 32:14}
-Sbox = (
-            0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
-            0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
-            0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15,
-            0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75,
-            0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84,
-            0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF,
-            0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8,
-            0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2,
-            0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73,
-            0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
-            0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79,
-            0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08,
-            0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
-            0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
-            0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
-            0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16,
-        )
+class PGPConfig:
+    def __init__(self, hashfunc, symmetic_func, sym_key_length, crypted_obj, seed,sym_mode = 1):
+        self.hashfunc = hashfunc
+        self.symmetic_func = symmetic_func
+        self.crypto_obj = crypted_obj # 其值要么为string要么为file
+        self.key_length = sym_key_length
+        self.sym_mode = sym_mode # 0代表对称密钥由外部输入，而1代表由种子产生
+        self.seed = seed
 
-InvSbox = (
-            0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
-            0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
-            0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
-            0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
-            0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
-            0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
-            0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
-            0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
-            0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
-            0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
-            0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
-            0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
-            0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
-            0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
-            0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
-            0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D,
-        )
-Rcon = (
-            0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40,
-            0x80, 0x1B, 0x36, 0x6C, 0xD8, 0xAB, 0x4D, 0x9A,
-            0x2F, 0x5E, 0xBC, 0x63, 0xC6, 0x97, 0x35, 0x6A,
-            0xD4, 0xB3, 0x7D, 0xFA, 0xEF, 0xC5, 0x91, 0x39,
-        )
+class PGPSender(PGPConfig):
+    def __init__(self, hashfunc, symmetic_func, sym_key_length, crypted_obj, seed ,text, sym_mode = 1):
+        super().__init__(hashfunc, symmetic_func, sym_key_length, crypted_obj, seed, sym_mode)
+        if len(text) == 0:
+            raise Exception("Empty text Error, Enter text")
+        self.text = text
+        self.pr_key_sender = [] # 发送方私钥
+        self.pub_key_sender = [] # 发送方公钥
+        self.pub_key_recipient = [] # 接收方公钥
+        self.sym_key = ""
+        self.sig_field_length = 0 # 签名长度
+        self.sym_key_field_length = 0 # 对称密钥域长度
+        self.ispad = False
+        
+    def keyGenerate(self):
+        RSAkey = RSA.get_RSAKey()
+        Pub_key = RSAkey['puk'] # Pub = [n, e]
+        Pr_key = RSAkey['prk'] # Pr = [n, d]
+        self.pub_key_sender = Pub_key
+        self.pr_key_sender = Pr_key
+        random.seed(self.seed)
+        Sym_key = random.randint(2**(self.key_length-1), 2**self.key_length)
+        symkey = AllConvert.numtohex(Sym_key)
+        print("symkey:", symkey)
+        self.sym_key = symkey
+
+    def stringCrypt(self): # 加密前需要生成密钥，并交换公钥, block是128bit的16进制字符串
+        if len(self.pr_key_sender) == 0  \
+                or len(self.pub_key_sender) == 0  \
+                or len(self.pub_key_recipient) == 0:
+            raise Exception("No asymmetric key detected, generate it first")
+
+        hashed = ""
+        if self.hashfunc == 'MD5':
+            hashed = MD5.MD5.hash(self.text)
+        elif self.hashfunc == 'SHA':
+            hashed = SHA.sha1(self.text)
+        print('-' * 30)
+        print("Hashing completed")
+        print(self.hashfunc + "HashValue: ", hashed)
+
+        hashed_num = AllConvert.hextonum(hashed)
+        signed_num = RSA.encryption(hashed_num, self.pr_key_sender) # 这里是签名
+        signed = AllConvert.numtohex(signed_num)
+        print('-'*30)
+        print("Signing completed")
+        print("SignValue: ", signed)
+        self.sig_field_length = len(signed)
 
 
+        plaintextWithSig = AllConvert.asciistrtohex(self.text) + signed
 
-def text2matrix(text, length = 16): # length给出text的长度有16，24，32字节可供选择，默认是16字节
-    matrix = []
-    for i in range(length):
-        byte = (text >> (8 * (length - 1 - i))) & 0xFF
-        if i % 4 == 0:
-            matrix.append([byte])
+        sym_crypted = "" # 对称加密后的数据
+        if self.symmetic_func == 'AES':
+            stringChunkSize = 128 // 4
+            for i in range(0, len(plaintextWithSig), stringChunkSize):
+                chunked = plaintextWithSig[i:i+stringChunkSize] if i+stringChunkSize < len(plaintextWithSig) else \
+                    plaintextWithSig[i:]
+                aes = AES.AES(master_key = self.sym_key, key_length = self.key_length // 8)
+                if len(chunked) < stringChunkSize:
+                    sym_crypted += aes.encrypt(chunked, padding=True)
+                    self.ispad = True
+                else:
+                    sym_crypted += aes.encrypt(chunked, padding=False)
+        if self.symmetic_func == 'DES':
+            stringChunkSize = 64 // 4
+
+            for i in range(0, len(plaintextWithSig), stringChunkSize):
+                chunked = plaintextWithSig[i:i+stringChunkSize] if i+stringChunkSize < len(plaintextWithSig) else \
+                    plaintextWithSig[i:]
+                des = DES.des()
+                if len(chunked) < stringChunkSize:
+                    sym_crypted += des.encrypt(self.sym_key,chunked, padding=True)
+                else:
+                    sym_crypted += des.encrypt(self.sym_key,chunked, padding=False)
+        print('-'*30)
+        print(self.symmetic_func + "symmetric encrypt completed!")
+
+        sym_key_num_form = AllConvert.hextonum(self.sym_key)
+
+        pub_encrypted_sym_key = RSA.encryption(sym_key_num_form, self.pub_key_recipient)
+        # 需要padding
+        self.sym_key_field_length = len(AllConvert.numtohex(pub_encrypted_sym_key))
+        result = sym_crypted + AllConvert.numtohex(pub_encrypted_sym_key)
+        return result
+
+
+class PGPRecipient(PGPConfig):
+    def __init__(self, hashfunc, symmetic_func, sym_key_length, crypted_obj, seed, text="", sym_mode=1):
+        super().__init__(hashfunc, symmetic_func, sym_key_length, crypted_obj, seed, sym_mode)
+        if len(text) == 0:
+            raise Exception("Empty text Error, Enter text")
+        self.text = text
+        self.pr_key_receiver = []  # 发送方私钥
+        self.pub_key_receiver = []  # 发送方公钥
+        self.pub_key_sender = []  # 接收方公钥
+        self.sym_key = ""
+        self.sig_field_length = 0 # 签名长度
+        self.sym_key_field_length = 0 # 对称密钥域长度
+        self.ispad = False
+
+    def keyGenerate(self):
+        RSAkey = RSA.get_RSAKey()
+        Pub_key = RSAkey['puk']  # Pub = [n, e]
+        Pr_key = RSAkey['prk']  # Pr = [n, d]
+        self.pub_key_receiver = Pub_key
+        self.pr_key_receiver = Pr_key
+
+    def stringDecrypt(self, hex_text_crypted):
+        self.text = hex_text_crypted
+        if len(self.pr_key_receiver) == 0  \
+                or len(self.pub_key_receiver) == 0  \
+                or len(self.pub_key_sender) == 0:
+            raise Exception("No asymmetric key detected, generate it first")
+
+        sym_key_crypted = self.text[-self.sym_key_field_length:]
+        crypted_text = self.text[0:-self.sym_key_field_length]
+
+        sym_key = RSA.decryption(AllConvert.hextonum(sym_key_crypted), self.pr_key_receiver)
+        self.sym_key = AllConvert.numtohex(sym_key)
+        print("sym_key", self.sym_key)
+
+        sym_decrypted = ""  # 对称解密后的数据
+        if self.symmetic_func == 'AES':
+            chunk_size =  128 // 4
+            aes = AES.AES(master_key=self.sym_key, key_length=len(self.sym_key) // 2)
+            for i in range(0, len(crypted_text), chunk_size):
+                if i + chunk_size < len(crypted_text):
+                    sym_decrypted += aes.decrypt(crypted_text[i:i+chunk_size])
+                elif self.ispad:
+                    sym_decrypted += aes.decrypt(crypted_text[i:], padding=True)
+
+        if self.symmetic_func == 'DES':
+            chunk_size =  64 // 4
+            des = DES.des()
+            for i in range(0, len(crypted_text), chunk_size):
+                if i + chunk_size < len(crypted_text):
+                    sym_decrypted += des.decrypt(self.sym_key, crypted_text[i:i+chunk_size])
+                elif self.ispad:
+                    sym_decrypted += des.decrypt(self.sym_key, crypted_text[i:], padding=True)
+
+        sig_field_encrypted = sym_decrypted[-self.sig_field_length:]
+        message_hex = sym_decrypted[0:-self.sig_field_length]
+        message_asc = AllConvert.hextoasciiStr(message_hex)
+
+        sig_field_encrypted_num_form = AllConvert.hextonum(sig_field_encrypted)
+        sig_num_form = RSA.decryption(sig_field_encrypted_num_form, self.pub_key_sender)
+        sig = AllConvert.numtohex(sig_num_form)
+        hashed = ""
+        if self.hashfunc == 'MD5':
+            hashed = MD5.MD5.hash(message_asc)
+        elif self.hashfunc == 'SHA':
+            hashed = SHA.sha1(message_asc)
+
+        if sig == hashed:
+            print("integrity checked: no deletion")
         else:
-            matrix[i // 4].append(byte)
-    return matrix
+            print("file changed!")
+            return
+        return message_asc
 
 
-def matrix2text(matrix, length = 16): # length的作用同上,length = 16,24,32
-    text = 0
-    for i in range(length // 4):
-        for j in range(4):
-            text |= (matrix[i][j] << (8 * (length - 1) - 8 * (4 * i + j)))
-    return text
+def switchKey(sender:PGPSender, receiver:PGPRecipient):
+    receiver.pub_key_sender = sender.pub_key_sender
+    sender.pub_key_recipient = receiver.pub_key_receiver
 
+def configTransmit(sender:PGPSender, receiver:PGPRecipient):
+    receiver.sig_field_length = sender.sig_field_length
+    receiver.sym_key_field_length = sender.sym_key_field_length
+    receiver.ispad = sender.ispad
 
-class AES:
-    def __init__(self, master_key, key_length = 16):
-        self.round_keys = []
-        self.key_length = key_length
-        self.roundnum = keyLength2roundNum[self.key_length]
-        self.change_key(master_key, self.key_length)
-        self.cipher_state = []
-        self.plain_state = []
-
-    def change_key(self, master_key, key_length = 16):
-        self.round_keys = text2matrix(master_key, key_length)
-        key_wordnum = self.key_length // 4 # 4个字节组成一个字，key_wordnum表示有几个字
-        for i in range(key_wordnum, 4 * (self.roundnum + 1)):
-            self.round_keys.append([])
-            if i % key_wordnum == 0: #处理需要经g函数处理的字
-                byte = self.round_keys[i - key_wordnum][0]        \
-                     ^ Sbox[self.round_keys[i - 1][1]]  \
-                     ^ Rcon[i // key_wordnum] # 这是最左边的字节,需要异或轮常量
-                self.round_keys[i].append(byte)
-
-                for j in range(1, 4): #处理需要经g函数处理的字的其他字节
-                    byte = self.round_keys[i - key_wordnum][j]    \
-                         ^ Sbox[self.round_keys[i - 1][(j + 1) % 4]]
-                    self.round_keys[i].append(byte)
-
-            elif self.key_length == 32 and i % key_wordnum == 4: # aes-256的密钥扩展较为特殊
-                for j in range(4):
-                    byte = self.round_keys[i - key_wordnum][j]    \
-                         ^ Sbox[self.round_keys[i - 1][j]]
-                    self.round_keys[i].append(byte)
-
-            else:
-                for j in range(4):
-                    byte = self.round_keys[i - key_wordnum][j]    \
-                         ^ self.round_keys[i - 1][j]
-                    self.round_keys[i].append(byte)
-
-        # for i in range(4, 4 * 11):
-        #     self.round_keys.append([])
-        #     if i % 4 == 0:
-        #         byte = self.round_keys[i - 4][0]        \
-        #              ^ Sbox[self.round_keys[i - 1][1]]  \
-        #              ^ Rcon[i // 4]
-        #         self.round_keys[i].append(byte)
-        #
-        #         for j in range(1, 4):
-        #             byte = self.round_keys[i - 4][j]    \
-        #                  ^ Sbox[self.round_keys[i - 1][(j + 1) % 4]]
-        #             self.round_keys[i].append(byte)
-        #     else:
-        #         for j in range(4):
-        #             byte = self.round_keys[i - 4][j]    \
-        #                  ^ self.round_keys[i - 1][j]
-        #             self.round_keys[i].append(byte)
-
-
-    def encrypt(self, plaintext):
-        self.plain_state = text2matrix(plaintext)
-
-        self.__add_round_key(self.plain_state, self.round_keys[:4])
-
-        for i in range(1, self.roundnum):
-            self.__round_encrypt(self.plain_state, self.round_keys[4 * i : 4 * (i + 1)])
-
-        self.__sub_bytes(self.plain_state)
-        self.__shift_rows(self.plain_state)
-        self.__add_round_key(self.plain_state, self.round_keys[4 * self.roundnum :])
-
-        return matrix2text(self.plain_state)
-
-    def decrypt(self, ciphertext):
-        self.cipher_state = text2matrix(ciphertext)
-
-        self.__add_round_key(self.cipher_state, self.round_keys[4 * self.roundnum:])
-        self.__inv_shift_rows(self.cipher_state)
-        self.__inv_sub_bytes(self.cipher_state)
-
-        for i in range(self.roundnum - 1, 0, -1):
-            self.__round_decrypt(self.cipher_state, self.round_keys[4 * i : 4 * (i + 1)])
-
-        self.__add_round_key(self.cipher_state, self.round_keys[:4])
-
-        return matrix2text(self.cipher_state)
-
-    def __add_round_key(self, s, k):
-        for i in range(4):
-            for j in range(4):
-                s[i][j] ^= k[i][j]
-
-
-    def __round_encrypt(self, state_matrix, key_matrix):
-        self.__sub_bytes(state_matrix)
-        self.__shift_rows(state_matrix)
-        self.__mix_columns(state_matrix)
-        self.__add_round_key(state_matrix, key_matrix)
-
-
-    def __round_decrypt(self, state_matrix, key_matrix):
-        self.__add_round_key(state_matrix, key_matrix)
-        self.__inv_mix_columns(state_matrix)
-        self.__inv_shift_rows(state_matrix)
-        self.__inv_sub_bytes(state_matrix)
-
-    def __sub_bytes(self, s):
-        for i in range(4):
-            for j in range(4):
-                s[i][j] = Sbox[s[i][j]]
-
-
-    def __inv_sub_bytes(self, s):
-        for i in range(4):
-            for j in range(4):
-                s[i][j] = InvSbox[s[i][j]]
-
-
-    def __shift_rows(self, s):
-        s[0][1], s[1][1], s[2][1], s[3][1] = s[1][1], s[2][1], s[3][1], s[0][1]
-        s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
-        s[0][3], s[1][3], s[2][3], s[3][3] = s[3][3], s[0][3], s[1][3], s[2][3]
-
-
-    def __inv_shift_rows(self, s):
-        s[0][1], s[1][1], s[2][1], s[3][1] = s[3][1], s[0][1], s[1][1], s[2][1]
-        s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
-        s[0][3], s[1][3], s[2][3], s[3][3] = s[1][3], s[2][3], s[3][3], s[0][3]
-
-    def __mix_single_column(self, a):
-        # please see Sec 4.1.2 in The Design of Rijndael
-        t = a[0] ^ a[1] ^ a[2] ^ a[3]
-        u = a[0]
-        a[0] ^= t ^ xtime(a[0] ^ a[1])
-        a[1] ^= t ^ xtime(a[1] ^ a[2])
-        a[2] ^= t ^ xtime(a[2] ^ a[3])
-        a[3] ^= t ^ xtime(a[3] ^ u)
-
-
-    def __mix_columns(self, s):
-        for i in range(4):
-            self.__mix_single_column(s[i])
-
-
-    def __inv_mix_columns(self, s):
-        # see Sec 4.1.3 in The Design of Rijndael
-        for i in range(4):
-            u = xtime(xtime(s[i][0] ^ s[i][2]))
-            v = xtime(xtime(s[i][1] ^ s[i][3]))
-            s[i][0] ^= u
-            s[i][1] ^= v
-            s[i][2] ^= u
-            s[i][3] ^= v
-
-        self.__mix_columns(s)
 if __name__ == '__main__':
-    # trans_mat = text2matrix(0x2b7e151628aed2a6abf7158809cf4f3c579c2b2f3b8d22834455667788990044, length=32)
-    # trans_num = matrix2text(trans_mat, length=32)
-    # print(hex(trans_num))
-
-    # aesobj = AES(master_key = 0x2b7e151628aed2a6abf7158809cf4f3c2b7e151628aed2a6, key_length=24)
-    # print(len(aesobj.round_keys))
-    # aesobj2 = AES(master_key = 0x2b7e151628aed2a6abf7158809cf4f3c2b7e151628aed2a6abf7158809cf4f3c, key_length=32)
-    # print(len(aesobj2.round_keys))
-    aesobj3 = AES(master_key = 0x2b7e151628aed2a6abf7158809cf4f3c2b7e151628aed2a6, key_length=24)
-    encryped = aesobj3.encrypt(0x3243f6a8885a308d313198a2e0370734)
-    print(hex(encryped))
-
+    hashfunc, symmetic_func, sym_key_length, crypted_obj, seed, text, sym_mode = \
+        ['MD5', 'AES', 128, 'string', 20, 'nmslwq', 1]
+    ps = PGPSender(hashfunc, symmetic_func, sym_key_length, crypted_obj, seed, text, sym_mode)
+    pr = PGPRecipient(hashfunc, symmetic_func, sym_key_length, crypted_obj, seed, text, sym_mode)
+    ps.keyGenerate()
+    pr.keyGenerate()
+    switchKey(ps, pr)
+    res = ps.stringCrypt()
+    configTransmit(ps, pr)
+    res2 = pr.stringDecrypt(res)
+    if res2:
+        print(res2)
+    # for i in range(0, len(res), 20):
+    #     print(res[i:i+20])
